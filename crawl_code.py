fc773a8f01106df460e1ba6a10d1a1e069bd5a5a
doanhieu1986 <doanhieuneu@gmail.com>
2024-06-09 05:33:53 +0700

    commit

--- crawl_code.py
+++ crawl_code.py
@@ -10,32 +10,128 @@ import glob
 from requests.exceptions import ConnectionError, Timeout
 import utils
 
-base_url = 'https://cafef.vn/timelinelist/18836/{}.chn'                         # change number 18833 or something like that to change topic
-output_file = '/Users/hieudoan/PROJECTS/INDEX 2023//result/cafef_18836.csv'     # Path to the CSV file storing the DataFrame with specific topic
+def crawl_cafef (base_url, output_file, start_page, end_page):
+    # base_url: url with format that need to be crawled
+    # output_file: path file and name file that crawled result will be saved to
+    # start_page, end_page: the range of pages that need to be crawled
 
-# Check if the CSV file exists
-if os.path.exists(output_file):
-    # Read the DataFrame from the CSV file
-    df_cafef = pd.read_csv(output_file)
-else:
-    # If the file doesn't exist, create a new DataFrame
-    df_cafef = pd.DataFrame(columns=['PAPER', 'NEWS', 'URL', 'TIME', 'CONTENT'])
+    # Check if the CSV file exists
+    if os.path.exists(output_file):
+        # Read the DataFrame from the CSV file
+        df_cafef = pd.read_csv(output_file)
+    else:
+        # If the file doesn't exist, create a new DataFrame
+        df_cafef = pd.DataFrame(columns=['PAPER', 'NEWS', 'URL', 'TIME', 'CONTENT'])
 
-for page in range(1, 501):  # Change the loop range as needed (325, 328 missing)
-    url = base_url.format(page)
-    print('--------Python is reading for url page', page)
+    for page in range(start_page, end_page + 1):  # Change the loop range as needed (325, 328 missing)
+        url = base_url.format(page)
+        print('--------Python is reading Cafef website for the page: ', page)
+        
+        try:
+            new_data = utils.df_cafef(url)  # Fetch data using the utils.df_cafef function
+            df_cafef = pd.concat([df_cafef, new_data], ignore_index=True)  # Add new data to the df_cafef DataFrame
+            time.sleep(3)
+            
+            # Save the DataFrame to a CSV file after each loop iteration
+            df_cafef.to_csv(output_file, index=False)
+            
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            print("Continuing to the next page...")
+
+    # Display the DataFrame after updating
+    print(df_cafef)
+
+# crawl_cafef ('https://cafef.vn/timelinelist/18836/{}.chn', '/Users/hieudoan/PROJECTS/INDEX 2023//result/cafef_18836.csv', 328, 328)
+# 18836: Doanh nghiep -- 18833: Vi~ mo -- 18834: Tai chinh ngan hang -- 18839: Thi truong
+
+def crawl_mof (base_url, output_file, start_page, end_page):
+    # base_url: url with format that need to be crawled
+    # output_file: path file and name file that crawled result will be saved to
+    # start_page, end_page: the range of pages that need to be crawled
+
+    # Check if the CSV file exists
+    if os.path.exists(output_file):
+        # Read the DataFrame from the CSV file
+        df = pd.read_csv(output_file)
+    else:
+        # If the file doesn't exist, create a new DataFrame
+        df = pd.DataFrame(columns=['PAPER', 'NEWS', 'URL', 'TIME', 'CONTENT'])
+
+    for page in range(start_page, end_page + 1):  # Change the loop range as needed (325, 328 missing)
+        url = base_url.format(page)
+        print('--------Python is reading MOF website for the page: ', page)
+        
+        try:
+            new_data = utils.df_mof(url)  # Fetch data using the utils.df_mof function
+            new_data['TIME'] = new_data['TIME'].apply(lambda x: pd.to_datetime(x, format='%d/%m/%Y %H:%M:%S').strftime('%Y-%m-%d'))     # convert time format 
+            df = pd.concat([df, new_data], ignore_index=True)  # Add new data to the df_cafef DataFrame
+            df.drop_duplicates(inplace=True)    # Drop duplicate rows based on the 'NEWS' and 'URL' columns
+            df.sort_values(by='TIME', ascending=False, inplace=True)
+            time.sleep(3)
+            
+            # Save the DataFrame to a CSV file after each loop iteration
+            df.to_csv(output_file, index=False)
+            
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            print("Continuing to the next page...")
+
+    # Display the DataFrame after updating
+    print(df)
+
+crawl_mof('https://mof.gov.vn/webcenter/portal/tttc/pages_r/o/hdntc?selectedPage={}&docType=TinBai&mucHienThi=1902', 
+          '/Users/hieudoan/PROJECTS/INDEX 2023//result/mof_hdntc.csv',2,2)
+
+def crawl_moit (base_url, output_file, start_page, end_page):
+    # base_url: url with format that need to be crawled
+    # output_file: path file and name file that crawled result will be saved to
+    # start_page, end_page: the range of pages that need to be crawled
     
-    try:
-        new_data = utils.df_cafef(url)  # Fetch data using the utils.df_cafef function
-        df_cafef = pd.concat([df_cafef, new_data], ignore_index=True)  # Add new data to the df_cafef DataFrame
-        time.sleep(3)
-        
-        # Save the DataFrame to a CSV file after each loop iteration
-        df_cafef.to_csv(output_file, index=False)
-        
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        print("Continuing to the next page...")
+    error_page = []
 
-# Display the DataFrame after updating
-print(df_cafef)
+    # Check if the CSV file exists
+    if os.path.exists(output_file):
+        # Read the DataFrame from the CSV file
+        df = pd.read_csv(output_file)
+    else:
+        # If the file doesn't exist, create a new DataFrame
+        df = pd.DataFrame(columns=['PAPER', 'NEWS', 'URL', 'TIME', 'CONTENT'])
+
+    for page in range(start_page, end_page + 1):  # Change the loop range as needed (325, 328 missing)
+        print('--------Python is reading MOIT website for the page: ', page)
+        
+        try:
+            new_data = utils.df_moit(base_url, page)  # Fetch data using the utils.df_mof function
+            # new_data['TIME'] = new_data['TIME'].apply(lambda x: pd.to_datetime(x, format='%d/%m/%Y').strftime('%Y-%m-%d'))     # convert time format 
+            df = pd.concat([df, new_data], ignore_index=True)  # Add new data to the df_cafef DataFrame
+            df.drop_duplicates(inplace=True)    # Drop duplicate rows based on the 'NEWS' and 'URL' columns
+            df.sort_values(by='TIME', ascending=False, inplace=True)
+            if len(new_data) == 0:
+                error_page.append(page)
+            time.sleep(10)
+            
+            # Save the DataFrame to a CSV file after each loop iteration
+            df.to_csv(output_file, index=False)
+            
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            print("Continuing to the next page...")
+
+    # Display the DataFrame after updating
+    print(df)
+    print('Error pages are (pages are not to be crawled): ', error_page)
+
+# Need update payload for each subjects on moit.gov.vn on utils.df_moit (see appendix payload file)
+# crawl_moit("https://moit.gov.vn/tin-tuc/phat-trien-cong-nghiep", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_ptcn.csv",4,4)
+# crawl_moit("https://moit.gov.vn/tin-tuc/thi-truong-trong-nuoc", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_tttn.csv",16,20)
+# crawl_moit("https://moit.gov.vn/tin-tuc/thi-truong-nuoc-ngoai", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_ttnn.csv",30,35)
+# crawl_moit("https://moit.gov.vn/tin-tuc/xuc-tien-thuong-mai", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_xttm.csv",30,30)
+# crawl_moit("https://moit.gov.vn/tin-tuc/bao-chi-voi-nguoi-dan", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_bcvnd.csv",9,10)
+# crawl_moit("https://moit.gov.vn/bao-ve-nen-tang-tu-tuong-cua-dang", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_bvntttd.csv",1,5)
+# crawl_moit("https://moit.gov.vn/tin-tuc/thong-bao", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_tb.csv",26,27)
+# crawl_moit("https://moit.gov.vn/quan-ly-thi-truong", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_qltc.csv",8,8)
+# crawl_moit("https://moit.gov.vn/tu-hao-hang-viet-nam", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_thhvn.csv",9,10)
+# crawl_moit("https://moit.gov.vn/phat-trien-ben-vung", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_ptbv.csv",6,9)
+# crawl_moit("https://moit.gov.vn/tin-tuc/chuyen-doi-so", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_cds.csv",1,10)
+# "https://moit.gov.vn/khoa-hoc-va-cong-nghe", "/Users/hieudoan/PROJECTS/INDEX 2023//result/moit_KHCN.csv",7,10
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
+import time
 
 # def get content for cafef.vn news
 def cafef_content(url, max_retries=10, retry_delay=3):
@@ -81,3 +82,175 @@ def df_cafef(url):
                 "CONTENT": CONTENT
                     }
     return pd.DataFrame(df_cafef)
+
+# def get content for mof.gov.vn news
+def mof_content(url, max_retries=10, retry_delay=3):
+    content_list = []
+    publish_date = []
+    retries = 0
+    paper_link = 'https://mof.gov.vn' + url
+    while retries < max_retries:
+        try:
+            response = requests.get(paper_link)
+            bts = BeautifulSoup(response.content, "html.parser")
+                
+            for content in bts.find_all('p'):
+                content_list.append(content.text.strip())
+            
+            for date_time in bts.find('small'):
+                publish_date.append(date_time.text.strip())
+                
+            break  # Successfully retrieved the content, exit the loop
+        except (ConnectionError, Timeout) as e:
+            print(f"ConnectionError occurred: {e}. Retrying in {retry_delay} seconds...")
+            retries += 1
+            time.sleep(retry_delay)
+
+    return ' '.join(content_list), ''.join(publish_date)
+
+# def format date for mof.gov.vn news
+def format_date_mof (date_string):
+    # Remove trailing whitespace from the date string
+    date_string = date_string.strip()
+    date_format = '%d/%m/%Y %H:%M:%S'
+    date_format = '%d/%m/%Y %H:%M:%S'
+
+    # Convert the string to a datetime object
+    date_object = datetime.strptime(date_string, date_format)
+
+    # Extract the date from the datetime object
+    formatted_date = date_object.strftime('%Y-%m-%d')
+    return formatted_date
+
+# def create dataframe for mof.gov.vn news
+def df_mof(url):
+    PAPER = []
+    NEWS = []
+    URL = []
+    TIME = []
+    CONTENT = []
+    time_new = ''
+    main_text = ''
+
+    response = requests.get(url)   
+    bts = BeautifulSoup(response.content, "html.parser")
+    for item in bts.find_all('h4'):
+        item_url = item.a['href']
+        try:
+            content_all = mof_content(item_url)                                 # get all content include time and main text
+            main_text = content_all[0].strip()                                  # get content of new
+            time_new = content_all[1] if len(content_all[1]) > 0 else ''        # get published date or time of new
+            if content_all is not None:
+                print('-----------------------------------reading content sequence:', len(main_text))
+        except Exception as e:
+            print(f"An error occurred for URL: {url}. Skipping this row.")
+            print(f"Error: {str(e)}")
+        
+        PAPER.append('mof.gov.vn')    # append default source from cafef.vn
+        NEWS.append(item.a.text)    # append Tiltle of new
+        URL.append(item_url)        # append link of new
+        TIME.append(time_new)
+        CONTENT.append(main_text)
+        time.sleep(2)           # 2-second delay between iterations
+    
+    df_mof = { 
+                "PAPER": PAPER, 
+                "NEWS": NEWS, 
+                "URL": URL,
+                "TIME": TIME,
+                "CONTENT": CONTENT
+                    }
+    return pd.DataFrame(df_mof)
+
+# def get content for moit.gov.vn news
+def moit_content(url, max_retries=10, retry_delay=3):
+    content_list = []
+    retries = 0
+
+    while retries < max_retries:
+        try:
+            response = requests.get(url)
+            bts = BeautifulSoup(response.content, "html.parser")
+            
+            for content in bts.find_all('p'):
+                content_list.append(content.text)
+            break  # Successfully retrieved the content, exit the loop
+        except (ConnectionError, Timeout) as e:
+            print(f"ConnectionError occurred: {e}. Retrying in {retry_delay} seconds...")
+            retries += 1
+            time.sleep(retry_delay)
+
+    return ' '.join(content_list)
+
+# def create dataframe for moit.gov.vn news
+
+def df_moit(url, page_no):
+    PAPER = []
+    NEWS = []
+    URL = []
+    TIME = []
+    CONTENT = []
+
+    payload = {
+    "layout": "Content.Article.News.default",
+    "itemsPerPage": 12,
+    "orderBy": "publishTime DESC",
+    "pageNo": page_no,
+    "service": "Content.Article.selectAll",
+    "widgetCode": "5b72a94b9218655475508114",
+    "parentId": 5238203,
+    "type": "Article.News",
+    "categoryId": 5238203,
+    "widgetTemplateId": "5feffbc0cccf1c7cdf7dada3",
+    "imageSizeRatio": "3:2",
+    "hiddenAuthor": 1,
+    "hiddenReadMore": 1,
+    "page": "Article.News.list",
+    "modulePosition": 23,
+    "moduleParentId": 12,
+    "phpModuleName": "Content.Listing",
+    "_t": 1717811711818
+}
+
+    response = requests.get(url, params=payload) 
+    bts = BeautifulSoup(response.content, "html.parser")
+    
+    for item in bts.find_all('a', class_='article-title common-title'):           
+        item_url = item.get('href')
+        next_div = item.find_next_sibling('div')  # Find the next sibling div
+        if next_div:
+            time_span = next_div.find('span', class_='article-date')  # Find the span within the div
+            if time_span:
+                time_new = time_span.get_text().strip()  # Get the text content of the found span
+            else:
+                time_new = 'Error in processing'  # Set to "Error in processing" if time_span is not found
+        else:
+            time_new = 'Error in processing'  # Set to "Error in processing" if next_div is not found
+        
+        try:
+            main_text = moit_content('https://moit.gov.vn' + item_url).strip()
+            if main_text is None or main_text == '':
+                main_text = 'Have an error in processing'
+            else:
+                print('-----------------------------------reading content sequence:', len(main_text))
+        except Exception as error:
+            print(f"An error occurred for URL: {url}. Skipping this row.")
+            print(f"Error: {str(error)}")
+            main_text = 'Have an error in processing'
+        
+        PAPER.append('moit.gov.vn')
+        NEWS.append(item.text)
+        URL.append('https://moit.gov.vn' + item_url)
+        TIME.append(time_new)
+        CONTENT.append(main_text)
+        # time.sleep(2)  # 1-second delay between iterations
+    
+    df_moit = { 
+                "PAPER": PAPER, 
+                "NEWS": NEWS, 
+                "URL": URL,
+                "TIME": TIME,
+                "CONTENT": CONTENT
+              }
+    return pd.DataFrame(df_moit)
+
